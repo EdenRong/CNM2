5325d92938207fc11fadfe429559c8e6414d79ea
eden <749988695@qq.com>
2016-04-07 14:29:38 +0800

    1.

---
+++ src/com/test/firstgithub/MyActivtity.java
@@ -0,0 +1,25 @@
+package com.test.firstgithub;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+public class MyActivtity extends Activity {
+
+	private TextView textview;
+	public int a;
+	public String b;
+	
+	
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.myactivity_layout);
+	}
+}
